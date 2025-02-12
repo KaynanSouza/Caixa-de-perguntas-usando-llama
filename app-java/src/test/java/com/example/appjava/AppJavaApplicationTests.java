4b61f67326a21a497b4cb99a66df847716e3deee
KaynanSouza <kaynansouza2005@gmail.com>
2025-02-11 23:01:12 -0300

    start project

---
+++ app-java/src/test/java/com/example/appjava/AppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
